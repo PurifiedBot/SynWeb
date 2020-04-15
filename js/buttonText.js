640b5768a5e0b18bfc15d07fa0622305dbfafe21
Purified <42700057+PurifiedBot@users.noreply.github.com>
2020-04-15 16:28:12 -0400

    Add files via upload

---
+++ js/buttonText.js
@@ -0,0 +1,6 @@
+function change() // no ';' here
+{
+    var elem = document.getElementById("footer1");
+    if (elem.value=="Hide Footer") elem.value = "Show Footer";
+    else elem.value = "Hide Footer";
+}
---
+++ js/toast.js
@@ -0,0 +1,11 @@
+$("#showAlert").click(function() {
+    $("#alert").toast("show");
+  });
+  
+  $("#showSuccess").click(function() {
+    $("#success").toast("show");
+  });
+  
+  $("#showInfo").click(function() {
+    $("#info").toast("show");
+  });
